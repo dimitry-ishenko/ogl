d9cd18df5e2dc4a31d725270f3a3474eaebce9ce
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-07-07 23:14:32 -0400

    color: add rgb() helper

--- color.hpp
+++ color.hpp
@@ -21,8 +21,10 @@ struct color : Vector<float, 4>
     constexpr auto&& a() const { return e[3]; }
 };
 
+constexpr auto rgb(float r, float g, float b) { return color{r, g, b, 1}; }
+
 }
 
-using ogl::color;
+using ogl::color, ogl::rgb;
 
 #endif
