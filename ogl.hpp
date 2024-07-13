8c38343b6c3ce3e5afb8aee5185d3aff556ed09c
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-07-12 20:00:29 -0400

    buffer -> vertex

--- ogl.hpp
+++ ogl.hpp
@@ -1,8 +1,8 @@
-#include <ogl/buffer.hpp>
 #include <ogl/color.hpp>
 #include <ogl/core.hpp>
 #include <ogl/error.hpp>
 #include <ogl/shader.hpp>
 #include <ogl/types.hpp>
 #include <ogl/vec.hpp>
+#include <ogl/vertex.hpp>
 #include <ogl/window.hpp>
--- src/vertex.cpp
+++ src/vertex.cpp
@@ -1,4 +1,4 @@
-#include <ogl/buffer.hpp>
+#include <ogl/vertex.hpp>
 #include <ogl/error.hpp>
 #include <ogl/extern.hpp>
 
