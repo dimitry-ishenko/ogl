fe09bb5a07741758f7856f77c1b6dd7852381ea6
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-07-14 13:43:17 -0400

    Redesign vertex_buffer
    Deleted several dependent types for now.

--- ogl/vertex.hpp
+++ ogl/vertex.hpp
@@ -10,75 +10,49 @@
 namespace ogl
 {
 
-template<typename R>
-concept Payload = std::ranges::contiguous_range<R> && std::ranges::sized_range<R>;
-
+namespace detail
+{
 class vertex_buffer
 {
+protected:
     unsigned vbo_;
+    size_t bytes_;
 
-    vertex_buffer(const void* payload, std::size_t size);
-
-    void bind();
-    static void unbind();
-    friend class vertex_array;
-
-public:
-    template<Payload P>
-    explicit vertex_buffer(P&& payload) :
-        vertex_buffer( std::data(payload), std::size(payload) * sizeof(decltype( *std::begin(payload) )) )
-    { }
+    vertex_buffer(const void* payload, size_t bytes);
     ~vertex_buffer();
 
     vertex_buffer(const vertex_buffer&) = delete;
     vertex_buffer& operator=(const vertex_buffer&) = delete;
 
-    vertex_buffer(vertex_buffer&& rhs) : vbo_{rhs.vbo_} { rhs.vbo_ = 0; }
-    vertex_buffer& operator=(vertex_buffer&& rhs)
-    {
-        vertex_buffer::~vertex_buffer();
-        vbo_ = rhs.vbo_;
-        rhs.vbo_ = 0;
-        return (*this);
-    }
+    vertex_buffer(vertex_buffer&&);
+    vertex_buffer& operator=(vertex_buffer&&);
+
+    auto bytes() const { return bytes_; }
+
+    void bind();
+    void unbind();
 };
+}
 
-struct vertex_attr
+template<typename R, typename V>
+concept Payload = std::ranges::contiguous_range<R> && std::ranges::sized_range<R> && std::same_as< std::ranges::range_value_t<R>, V >;
+
+template<typename V>
+class vertex_buffer : public detail::vertex_buffer
 {
-    vertex_buffer& buffer;
+public:
+    using value_type = V;
 
-    unsigned index;
-    ogl::count count;
-    ogl::type type;
-    ogl::norm norm = _dont_norm;
-    int stride = 0;
-    std::ptrdiff_t offset = 0;
+    template<Payload<V> P>
+    explicit vertex_buffer(P&& payload) :
+        detail::vertex_buffer{ std::data(payload), std::size(payload) * sizeof(V) }
+    { }
+
+    auto size() const { return bytes() / sizeof(value_type); }
 };
 
 template<typename R>
-concept VertexAttrs = std::ranges::range<R> && std::same_as< std::ranges::range_value_t<R>, vertex_attr >;
-
-using vertex_attrs = std::initializer_list<vertex_attr>;
-
-class vertex_array
-{
-    unsigned vao_;
-
-    vertex_array();
-    void enable_attr(const vertex_attr&);
-
-public:
-    template<VertexAttrs A>
-    vertex_array(A&& attrs) : vertex_array{ }
-    {
-        bind();
-        for (auto&& attr : attrs) enable_attr(attr);
-        unbind();
-    }
-
-    void bind();
-    static void unbind();
-};
+explicit vertex_buffer(R&&) -> vertex_buffer<std::ranges::range_value_t<R>>;
 
 }
 
--- src/vertex.cpp
+++ src/vertex.cpp
@@ -5,40 +5,38 @@
 namespace ogl
 {
 
-vertex_buffer::vertex_buffer(const void* payload, std::size_t size)
+namespace detail
+{
+
+vertex_buffer::vertex_buffer(const void* payload, size_t bytes) :
+    bytes_{bytes}
 {
     glGenBuffers(1, &vbo_);
     if (auto ev = glGetError()) throw opengl_error(ev);
 
     bind();
-    glBufferData(GL_ARRAY_BUFFER, size, payload, GL_STATIC_DRAW);
+
+    glBufferData(GL_ARRAY_BUFFER, bytes, payload, GL_STATIC_DRAW);
     if (auto ev = glGetError()) throw opengl_error(ev);
+
+    unbind();
 }
 
 vertex_buffer::~vertex_buffer() { glDeleteBuffers(1, &vbo_); }
 
+vertex_buffer::vertex_buffer(vertex_buffer&& rhs) : vbo_{rhs.vbo_} { rhs.vbo_ = 0; }
+
+vertex_buffer& vertex_buffer::operator=(vertex_buffer&& rhs)
+{
+    vertex_buffer::~vertex_buffer();
+    vbo_ = rhs.vbo_;
+    rhs.vbo_ = 0;
+    return (*this);
+}
+
 void vertex_buffer::bind() { glBindBuffer(GL_ARRAY_BUFFER, vbo_); }
 void vertex_buffer::unbind() { glBindBuffer(GL_ARRAY_BUFFER, 0); }
 
-vertex_array::vertex_array()
-{
-    glGenVertexArrays(1, &vao_);
-    if (auto ev = glGetError()) throw opengl_error(ev);
 }
-
-void vertex_array::enable_attr(const vertex_attr& attr)
-{
-    attr.buffer.bind();
-
-    glVertexAttribPointer(attr.index, attr.count, attr.type, attr.norm, attr.stride,
-        reinterpret_cast<void*>(attr.offset)
-    );
-    if (auto ev = glGetError()) throw opengl_error(ev);
-
-    glEnableVertexAttribArray(attr.index);
-}
-
-void vertex_array::bind() { glBindVertexArray(vao_); }
-void vertex_array::unbind() { glBindVertexArray(0); }
 
 }
