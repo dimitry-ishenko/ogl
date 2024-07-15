359187d63bd653b9ffe3d0cbe999042e63c848cf
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2024-07-14 22:51:55 -0400

    Simplify vertex_attr

--- ogl/vertex.hpp
+++ ogl/vertex.hpp
@@ -20,41 +20,32 @@ namespace ogl
 {
 
 ////////////////////////////////////////////////////////////////////////////////
-class shader_program;
-class vertex_attr;
-void draw(shader_program&, vertex_attr&, std::size_t from);
-
-namespace internal
-{
-class vertex_buffer
+class vertex_buffer_base
 {
 protected:
     unsigned vbo;
     std::size_t bytes;
 
-    vertex_buffer(const void* payload, std::size_t bytes);
-    ~vertex_buffer();
+    vertex_buffer_base(const void* payload, std::size_t bytes);
+    ~vertex_buffer_base();
 
-    vertex_buffer(const vertex_buffer&) = delete;
-    vertex_buffer& operator=(const vertex_buffer&) = delete;
+    vertex_buffer_base(const vertex_buffer_base&) = delete;
+    vertex_buffer_base& operator=(const vertex_buffer_base&) = delete;
 
-    vertex_buffer(vertex_buffer&&);
-    vertex_buffer& operator=(vertex_buffer&&);
+    vertex_buffer_base(vertex_buffer_base&&);
+    vertex_buffer_base& operator=(vertex_buffer_base&&);
 
     void bind();
     void unbind();
-
-    friend class ogl::vertex_attr;
-    friend void ogl::draw(shader_program&, vertex_attr&, std::size_t from);
+    friend class vertex_attr;
 };
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 template<typename R, typename V>
 concept Payload = std::ranges::contiguous_range<R> && std::ranges::sized_range<R> && std::same_as< std::ranges::range_value_t<R>, V >;
 
 template<typename V>
-class vertex_buffer : public internal::vertex_buffer
+class vertex_buffer : public vertex_buffer_base
 {
 public:
     using value_type = typename type_traits<V>::value_type;
@@ -66,7 +57,7 @@ public:
     static constexpr auto opengl_type = type_traits<V>::opengl_type;
 
     template<Payload<V> P>
-    explicit vertex_buffer(P&& payload) : internal::vertex_buffer{ std::data(payload), std::size(payload) * stride } { }
+    explicit vertex_buffer(P&& payload) : vertex_buffer_base{ std::data(payload), std::size(payload) * stride } { }
 };
 
 template<typename R>
@@ -76,31 +67,29 @@ explicit vertex_buffer(R&&) -> vertex_buffer<std::ranges::range_value_t<R>>;
 template<typename B>
 concept VertexBuffer = std::same_as<B, vertex_buffer<typename B::value_type>>;
 
+class shader_program;
+
 class vertex_attr
 {
-    internal::vertex_buffer* buf;
-
     unsigned index;
-    std::size_t element_size;
-    unsigned element_type;
-    ogl::norm norm;
-    std::size_t stride;
-    std::ptrdiff_t off;
+    std::size_t size;
 
-    void create();
+    vertex_attr(vertex_buffer_base&, unsigned index, std::size_t element_size, unsigned element_type, ogl::norm, std::size_t stride, std::ptrdiff_t off);
 
-    bool created = false;
-    void enable();
-    void disable();
+    void enable() const;
+    void disable() const;
 
-    friend void draw(shader_program&, vertex_attr&, std::size_t from);
-    friend void draw(shader_program&, vertex_attr&, std::size_t from, std::size_t size);
+    friend class vertex_array;
+    friend void draw(shader_program&, vertex_attr&, std::size_t);
+    friend void draw(shader_program&, vertex_attr&, std::size_t, std::size_t);
 
 public:
     template<VertexBuffer B>
     vertex_attr(B& buf, unsigned index, std::size_t element_size = B::element_size, std::ptrdiff_t off = 0, std::size_t stride = B::stride, ogl::norm norm = dont_norm) :
-        buf{&buf}, index{index}, element_size{element_size}, element_type{B::opengl_type}, norm{norm}, stride{stride}, off{off}
-    { }
+        vertex_attr{ buf, index, element_size, B::opengl_type, norm, stride, off }
+    {
+        size = buf.bytes / (stride ? stride : (element_size * sizeof(typename B::element_type)));
+    }
     ~vertex_attr() { disable(); }
 
     vertex_attr(const vertex_attr&) = delete;
--- src/vertex.cpp
+++ src/vertex.cpp
@@ -14,10 +14,7 @@ namespace ogl
 {
 
 ////////////////////////////////////////////////////////////////////////////////
-namespace internal
-{
-
-vertex_buffer::vertex_buffer(const void* payload, std::size_t bytes) : bytes{bytes}
+vertex_buffer_base::vertex_buffer_base(const void* payload, std::size_t bytes) : bytes{bytes}
 {
     glGenBuffers(1, &vbo);
     if (auto ev = glGetError()) throw opengl_error(ev);
@@ -30,70 +27,49 @@ vertex_buffer::vertex_buffer(const void* payload, std::size_t bytes) : bytes{byt
     unbind();
 }
 
-vertex_buffer::~vertex_buffer() { glDeleteBuffers(1, &vbo); }
+vertex_buffer_base::~vertex_buffer_base() { glDeleteBuffers(1, &vbo); }
 
-vertex_buffer::vertex_buffer(vertex_buffer&& rhs) : vbo{rhs.vbo} { rhs.vbo = 0; }
+vertex_buffer_base::vertex_buffer_base(vertex_buffer_base&& rhs) : vbo{rhs.vbo} { rhs.vbo = 0; }
 
-vertex_buffer& vertex_buffer::operator=(vertex_buffer&& rhs)
+vertex_buffer_base& vertex_buffer_base::operator=(vertex_buffer_base&& rhs)
 {
-    vertex_buffer::~vertex_buffer();
+    vertex_buffer_base::~vertex_buffer_base();
     vbo = rhs.vbo;
     rhs.vbo = 0;
     return (*this);
 }
 
-void vertex_buffer::bind() { glBindBuffer(GL_ARRAY_BUFFER, vbo); }
-void vertex_buffer::unbind() { glBindBuffer(GL_ARRAY_BUFFER, 0); }
-
-}
+void vertex_buffer_base::bind() { glBindBuffer(GL_ARRAY_BUFFER, vbo); }
+void vertex_buffer_base::unbind() { glBindBuffer(GL_ARRAY_BUFFER, 0); }
 
 ////////////////////////////////////////////////////////////////////////////////
-vertex_attr::vertex_attr(vertex_attr&& rhs) :
-    buf{rhs.buf}, index{rhs.index}, element_size{rhs.element_size}, element_type{rhs.element_type}, norm{rhs.norm}, stride{rhs.stride}, off{rhs.off}, created{rhs.created}
+namespace { static constexpr unsigned invalid_index = -1; }
+
+vertex_attr::vertex_attr(vertex_buffer_base& buf, unsigned index, std::size_t element_size, unsigned element_type, ogl::norm norm, std::size_t stride, std::ptrdiff_t off) :
+    index{index}
 {
-    rhs.buf = nullptr;
-    rhs.created = false;
+    buf.bind();
+
+    glVertexAttribPointer(index, element_size, element_type, norm, stride, reinterpret_cast<const void*>(off));
+    if (auto ev = glGetError()) throw opengl_error(ev);
+
+    buf.unbind();
 }
+
+vertex_attr::vertex_attr(vertex_attr&& rhs) : index{rhs.index} { rhs.index = invalid_index; }
 
 vertex_attr& vertex_attr::operator=(vertex_attr&& rhs)
 {
     vertex_attr::~vertex_attr();
 
-    buf = rhs.buf;
     index = rhs.index;
-    element_size = rhs.element_size;
-    element_type = rhs.element_type;
-    norm = rhs.norm;
-    stride = rhs.stride;
-    off = rhs.off;
-    created = rhs.created;
-
-    rhs.buf = nullptr;
-    rhs.created = false;
+    rhs.index = invalid_index;
 
     return (*this);
 }
 
-void vertex_attr::create()
-{
-    buf->bind();
-
-    glVertexAttribPointer(index, element_size, element_type, norm, stride, reinterpret_cast<const void*>(off));
-    if (auto ev = glGetError()) throw opengl_error(ev);
-
-    buf->unbind();
-}
-
-void vertex_attr::enable()
-{
-    if (!created) { create(); created = true; }
-    glEnableVertexAttribArray(index);
-}
-
-void vertex_attr::disable()
-{
-    if (created) glDisableVertexAttribArray(index);
-}
+void vertex_attr::enable() const { if (index != invalid_index) glEnableVertexAttribArray(index); }
+void vertex_attr::disable() const { if (index != invalid_index) glDisableVertexAttribArray(index); }
 
 ////////////////////////////////////////////////////////////////////////////////
 }
